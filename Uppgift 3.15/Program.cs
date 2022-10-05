96e53817428ffd0df17d50084274c928b4df72a7
KungeniDjungeln <thhe05001@utb.vaxjo.se>
2022-10-05 11:51:33 +0200

    Add project files.

---
+++ Uppgift 3.15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_3_13
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen");
+            int svar = int.Parse(Console.ReadLine());
+            int avgift = 80;
+            int pris = svar * avgift;
+
+
+            if (svar >= 1 && svar < 11)
+            {
+                Console.WriteLine("Det kommer att kosta " + pris);
+
+            }
+
+            else if (svar > 11)
+                Console.WriteLine("Du kan endast hyra en bil i tidsförloppet av 1-11 timmar");
+
+        }
+    }
+}
